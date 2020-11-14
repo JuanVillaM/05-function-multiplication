6807fc74b32b0bce4407d459b3825537ccb7d09f
Juan Villa <juanestebanvillamemdoza@gmail.com>
2020-11-14 19:03:27 -0400

    Agregando archivo app.js

---
+++ app.js
@@ -0,0 +1,27 @@
+function sumar(base, limite) {
+    for (let i = 1; i <= limite; i++) {
+        let resultado = base * i
+        console.log(base + " x " + i + " = " + resultado);
+    }
+
+}
+
+sumar(1, 10);
+console.log("===============");
+sumar(2, 10);
+console.log("===============");
+sumar(3, 10);
+console.log("===============");
+sumar(4, 10);
+console.log("===============");
+sumar(5, 10);
+console.log("===============");
+sumar(6, 10);
+console.log("===============");
+sumar(7, 10);
+console.log("===============");
+sumar(8, 10);
+console.log("===============");
+sumar(9, 10);
+console.log("===============");
+sumar(10, 10);
